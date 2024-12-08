af1ea57a1e5311a9a4f62158edf68f96ae9cc5a7
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-12-08 20:00:25 +0100

    Update log.py

--- src/EPGImport/log.py
+++ src/EPGImport/log.py
@@ -8,23 +8,24 @@
 import sys
 from cStringIO import StringIO
 import threading
-
+import os
 logfile = StringIO()
 # Need to make our operations thread-safe.
 mutex = threading.Lock()
 
 
 def write(data):
-    # with mutex:
-        # Check if the log exceeds 8 KB
-        # if logfile.tell() > 100000:
-            # # Move the pointer to the beginning
-            # logfile.seek(0)
-            # logfile.truncate(0)  # Clear the buffer
-        # logfile.write(data)
-        # logfile.write('\n')
-    # sys.stdout.write(data)
-
+    '''
+    with mutex:
+        Check if the log exceeds 8 KB
+        if logfile.tell() > 100000:
+            # Move the pointer to the beginning
+            logfile.seek(0)
+            logfile.truncate(0)  # Clear the buffer
+        logfile.write(data)
+        logfile.write('\n')
+    sys.stdout.write(data)
+    '''
     with mutex:
         if logfile.tell() > 1000000:  # 1 MB
             logfile.close()
@@ -35,20 +36,20 @@ def write(data):
                     i += 1
                 backup_name = backup_name + "." + str(i)
             os.rename(logfile.name, backup_name)
-            logfile = open(logfile.name, 'w')
+            # logfile = open(logfile.name, 'w')
             logfile.write("Logfile rotated\n")
         logfile.write(data)
         logfile.write('\n')
         try:
             sys.stdout.write(data + '\n')
         except Exception as e:
-            sys.stderr.write("Errore nella scrittura su stdout: " + str(e) + '\n')
+            sys.stderr.write("Error writing to stdout: " + str(e) + '\n')
 
 
 def getvalue():
     with mutex:
         if logfile.closed:
-            raise ValueError("Il file di log è chiuso e non può essere letto.")
+            print("The log file is closed and cannot be read.")
         # # Capture the current position
         # # pos = logfile.tell()
         logfile.seek(0)  # Move to the start of the buffer
